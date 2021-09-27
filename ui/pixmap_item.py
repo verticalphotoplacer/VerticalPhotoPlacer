15d3405b1fb997d0bd43015b61564bbff494e409
chuc92man <chuc92man@gmail.com>
2021-09-27 22:42:43 +0900

    Update pixmap_item.py

--- ui/pixmap_item.py
+++ ui/pixmap_item.py
@@ -1,8 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep 23 11:18:03 2020
-
-@author: chuc9
+/******************************************************************************************
+ VerticalPhotoPlacer
+ The Vertical Photo Placer Plugin for QGIS performs quick placement of
+ vertical drone photos on map.
+                              -------------------
+        begin                : 2019-09-05
+        copyright            : (C) 2019-2021 by Chubu University and
+               National Research Institute for Earth Science and Disaster Resilience (NIED)
+        email                : chuc92man@gmail.com
+ ******************************************************************************************/
+/******************************************************************************************
+ *   This file is part of Vertical Photo Placer Plugin.                                   *
+ *                                                                                        *
+ *   This program is free software; you can redistribute it and/or modify                 *
+ *   it under the terms of the GNU General Public License as published by                 *
+ *   the Free Software Foundation, version 3 of the License.                              *
+ *                                                                                        *
+ *   Vertical Photo Placer Plugin is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or    *
+ *   FITNESS FOR A PARTICULAR PURPOSE.                                                    *
+ *   See the GNU General Public License for more details.                                 *
+ *                                                                                        *
+ *   You should have received a copy of the GNU General Public License along with         *
+ *   Vertical Photo Placer Plugin. If not, see <http://www.gnu.org/licenses/>.            *
+ ******************************************************************************************/
 """
 
 from qgis.PyQt.QtWidgets import QGraphicsPixmapItem
